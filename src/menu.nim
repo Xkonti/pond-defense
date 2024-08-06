bc11a669e41bf5381019de98800fa664ac3e62ae
Xkonti <xkonti@xkonti.tech>
2024-08-06 01:48:26 -0400

    Make "you died" text more prominent

--- src/menu.nim
+++ src/menu.nim
@@ -2,17 +2,18 @@ var
   showMenu = true
 
 proc drawMenu(): bool =
-  if isFrogDead:
-    drawText("You died!", 10, 10, 20, Red)
-
-  # Draw the start/restart button
-  let startButtonText = if isFrogDead: "Play again" else: "Start game"
-
+  # Precalculate vertical button values
   const buttonHeight = 60
   const buttonGap = buttonHeight / 2
   const buttonsTotalHeight = buttonHeight * 2 + buttonGap
-
   const verticalStart = (screenHeight / 2) - (buttonsTotalHeight / 2)
+
+  if isFrogDead:
+    drawText("You died!", (screenWidth / 2 - 212).int32, (verticalStart - buttonHeight * 2 + 6).int32, 100, DarkGray)
+    drawText("You died!", (screenWidth / 2 - 218).int32, (verticalStart - buttonHeight * 2).int32, 100, White)
+
+  # Draw the start/restart button
+  let startButtonText = if isFrogDead: "Play again" else: "Start game"
 
   if (drawButton(Rectangle(x: screenWidth / 2 - 200, y: verticalStart, width: 400, height: 60), startButtonText)):
     resetFrog()
